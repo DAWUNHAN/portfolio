c44500bee22e4045a6eaa6c10c7cf925086292c2
D.A Han <dawun.han@gmail.com>
2019-07-24 22:53:42 +1000

    change background

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var $main_icon = $('.main-icon');
-var $win = $(window);
 
-$win.on('scroll', function(){
-  var top = $win.scrollTop();
-  $main_icon.css('transform', 'rotate(' + top + 'deg');
-})
+// var $win = $(window);
+//
+// $win.on('scroll', function(){
+//   var top = $win.scrollTop();
+//   $main_icon.css('transform', 'rotate(' + top + 'deg');
+// })
+
+$( document ).ready( function() {
+   $('.main-sentence-ready').fadeIn( 3000, 'linear' );
+ } );
