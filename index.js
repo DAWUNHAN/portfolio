b3ac56f6e07c1338a03327d8525c11e16bcecdee
D.A Han <dawun.han@gmail.com>
2019-07-25 20:32:34 +1000

    main textition jquery

--- index.js
+++ index.js
@@ -8,4 +8,13 @@
 
 $( document ).ready( function() {
    $('.main-sentence-ready').fadeIn( 3000, 'linear' );
+   $('.main-sentence-hello').textition({
+     speed: 1,
+     animation: 'ease-out',
+     map: {x: 400, y: 200, z: 0},
+     autoplay: true,
+     interval: 1.5,
+    perspective: 300
+   });
+
  } );
